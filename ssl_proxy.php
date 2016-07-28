09b6caaa820b5aa6e922bfac472ba12bfdc7ac93
Tony <kloddant@users.noreply.github.com>
2016-07-28 10:54:34 -0500

    Initial commit.

---
+++ ssl_proxy.php
@@ -0,0 +1,43 @@
+<?php
+
+	$url = $_GET['url'];
+	$url = strip_tags($url);
+	
+	$url_pieces = parse_url($url);
+	if (!isset($url_pieces['path'])) {
+		exit;
+	}
+	$scheme = (isset($url_pieces['scheme']) ? $url_pieces['scheme']."://" : '');
+	$host = (isset($url_pieces['host']) ? $url_pieces['host'] : '');
+	$path = $url_pieces['path'];
+
+	$path = urldecode($path);
+	$path = implode('/', array_map('rawurlencode', explode('/', $path)));
+	$url = $scheme.$host.$path;
+	
+	$url = filter_var($url, FILTER_SANITIZE_URL);
+	
+	if (filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED) === false) {
+		exit;
+	}
+
+	$curl_options = array(
+		CURLOPT_FAILONERROR => true,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_RETURNTRANSFER => false,
+		CURLOPT_SSL_VERIFYHOST => false,
+		CURLOPT_SSL_VERIFYPEER => false,
+		CURLOPT_HTTPGET => true,
+		CURLOPT_HEADER => false,
+		CURLOPT_VERBOSE => true,
+		CURLOPT_BINARYTRANSFER => true,
+		CURLOPT_TIMEOUT => 900,
+	);
+
+	$curl = curl_init($url);
+	curl_setopt_array($curl, $curl_options); 
+	header('Content-Type: '.curl_getinfo($curl, CURLINFO_CONTENT_TYPE));  
+	$result = curl_exec($curl);
+	curl_close($curl);
+	 
+?>
